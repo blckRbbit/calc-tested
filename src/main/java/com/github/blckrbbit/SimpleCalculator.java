ce1dcad322693dfbdfd6a70e6d4ce0a9ab469c08
blckRbbit <lex.leonidov@gmail.com>
2023-04-04 14:08:56 +0600

    Initial commit

---
+++ src/main/java/com/github/blckrbbit/SimpleCalculator.java
@@ -0,0 +1,11 @@
+package com.github.blckrbbit;
+
+public class SimpleCalculator {
+    public static double sum(double a, double b) {return a + b;}
+
+    public static double minus(double a, double b) {return a - b;}
+
+    public static double divide(double a, double b) {return a/b;}
+
+    public static double multiply(double a, double b) {return a * b;}
+}
